fbdbdb4519d1b03dd768913f9a73b50ca7de5901
1xcodex <yishak.ab1216@gmail.com>
2025-09-08 04:40:55 -0700

    some fixs on header and footer side

--- netflix-clone/src/components/Footer/Footer.jsx
+++ netflix-clone/src/components/Footer/Footer.jsx
@@ -1,58 +1,120 @@
-import React from 'react'
-import "./footer.css"
+// import React from 'react'
+// import "./footer.css"
+// import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+// import InstagramIcon from "@mui/icons-material/Instagram";
+// import YouTubeIcon from "@mui/icons-material/YouTube";
+// const Footer = () => {
+//   return (
+//     <div className='footer_outer_container'>
+//         <div className='footer_inner_container'>
+//             <div className='footer_icons'>
+//                 <FacebookOutlinedIcon />
+//                 <InstagramIcon />
+//                 <YouTubeIcon />
+//             </div>
+//             <div className='footer_data'>
+//                 <div>
+//                     <ul>
+//                         <li>Audio Description</li>
+//                         <li>Investor Relations</li>
+//                         <li>Legal Notice</li>
+//                     </ul>
+//                 </div>
+//                 <div>
+//                     <ul>
+//                         <li>Help Center</li>
+//                         <li>Jobs</li>
+//                         <li>Cookie Preferences</li>
+//                     </ul>
+//                 </div>
+//                 <div>
+//                     <ul>
+//                         <li href="https://www.w3schools.com">Gift Cards</li>
+//                         <li>Terms of Uses</li>
+//                         <li>Corporate Information</li>
+//                     </ul>
+//                 </div>
+//                 <div>
+//                     <ul>
+//                         <li>Media Center</li>
+//                         <li>Privacey</li>
+//                         <li>Contact Us</li>
+//                     </ul>
+//                 </div>
+//             </div>
+//             <div className='service_code'>
+//                 <p>
+//                     Service Code
+//                 </p>
+//             </div>
+//             <div className='copy-right'>
+//                 &copy; 1997-2025 Netflix, Inc.
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Footer
+
+
+
+import React from "react";
 import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import "./Footer.css";
+
 const Footer = () => {
   return (
-    <div className='footer_outer_container'>
-        <div className='footer_inner_container'>
-            <div className='footer_icons'>
-                <FacebookOutlinedIcon />
-                <InstagramIcon />
-                <YouTubeIcon />
-            </div>
-            <div className='footer_data'>
-                <div>
-                    <ul>
-                        <li>Audio Description</li>
-                        <li>Investor Relations</li>
-                        <li>Legal Notice</li>
-                    </ul>
-                </div>
-                <div>
-                    <ul>
-                        <li>Help Center</li>
-                        <li>Jobs</li>
-                        <li>Cookie Preferences</li>
-                    </ul>
-                </div>
-                <div>
-                    <ul>
-                        <li>Gift Cards</li>
-                        <li>Terms of Uses</li>
-                        <li>Corporate Information</li>
-                    </ul>
-                </div>
-                <div>
-                    <ul>
-                        <li>Media Center</li>
-                        <li>Privacey</li>
-                        <li>Contact Us</li>
-                    </ul>
-                </div>
-            </div>
-            <div className='service_code'>
-                <p>
-                    Service Code
-                </p>
-            </div>
-            <div className='copy-right'>
-                &copy; 1997-2025 Netflix, Inc.
-            </div>
+    <footer className="footer-outer-container">
+      <div className="footer-inner-container">
+        <div className="footer-icons">
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <TwitterIcon/>
+          <YouTubeIcon />
         </div>
-    </div>
-  )
-}
 
-export default Footer
+        <div className="footer-links-grid">
+          <div className="footer-column">
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Legal Notice</li>
+              <li>Do Not Sell or Share My Personal Information</li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <ul>
+              <li>Gift Cards</li>
+              <li>Netflix Shop</li>
+              <li>Cookie perferences</li>
+              <li>Ad Choices</li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <ul>
+              <li>Media Center</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="copyright">© 1997–2025 Netflix, Inc.</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- netflix-clone/src/components/Header/Header.jsx
+++ netflix-clone/src/components/Header/Header.jsx
@@ -11,35 +11,46 @@ const Header = () => {
     <div className="header_outer_container">
       <div className="header_container">
         <div className="header_left">
-          <ul>
-            <li><img src={Netflixlogo} alt="netflixlogo" width="100" /></li>
-            <li>Home</li>
-            <li>TVShows</li>
-            <li>Movies</li>
-            <li>Latest</li>
-            <li>MyLists</li>
-            <li>Browse by Languages</li>
-          </ul>
+          <div className="nav-links">
+            <ul>
+              <li>
+                <img src={Netflixlogo} alt="netflixlogo" width="100" />
+              </li>
+              <li>Home</li>
+              <li>TV Shows</li>
+              <li>Movies</li>
+              <li>Games</li>
+              <li>New & Popular</li>
+              <li>My List</li>
+              <li>Browse by Languages</li>
+            </ul>
+          </div>
         </div>
         <div className="header_right">
           <ul>
-            <li>
+            <li className="icon">
               <IoMenu />
             </li>
-            <li>
+            <li className="icon">
               <SearchIcon />
             </li>
-            <li>
+            <li className="icon">
               <NotificationsNoneIcon />
             </li>
-            <li>
+            <li className="icon">
               <AccountBoxIcon />
             </li>
-            <li>
+            <li className="icon">
               <ArrowDropDownIcon />
             </li>
           </ul>
         </div>
+        {/* <div className="header-right">
+            <SearchIcon className="icon" />
+            <NotificationsIcon className="icon" />
+            <AccountBoxIcon className="icon" />
+            <ArrowDropDownIcon className="icon" />
+        </div> */}
       </div>
     </div>
   );
